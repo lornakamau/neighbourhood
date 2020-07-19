36b81bc6fdc21e2072400c1679eddd46bf3030ca
Lorna Kamau <kamaulorna@gmail.com>
2020-07-19 23:27:40 +0300

    add create profile admin view function

--- neighbourhoodApp/views.py
+++ neighbourhoodApp/views.py
@@ -14,3 +14,19 @@ def email(request):
     send_signup_email_admin(name, email)
     return redirect(create_profile_admin)
 
+@login_required(login_url='/accounts/login/')
+def create_profile_admin(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = AdminProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.this_user = current_user
+            profile.save()
+        return redirect(create_hood)
+
+    else:
+        form = AdminProfileForm()
+      
+    title = "Admin profile "
+    return render(request, 'profile_admin.html', {"form": form, "title": title})
