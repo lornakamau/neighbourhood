76d98926a0cf0b3219b9adb7a5f60bfdf6669da1
Lorna Kamau <kamaulorna@gmail.com>
2020-07-19 11:09:22 +0300

    test admin and occupant models

--- neighbourhoodApp/models.py
+++ neighbourhoodApp/models.py
@@ -9,6 +9,13 @@ class Admin(models.Model):
     def __str__(self):
         return self.user.username
 
+class Occupant(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    profile_pic = CloudinaryField('Profile Picture')
+
+    def __str__(self):
+        return self.user.username
+
 class Neighbourhood(models.Model):
     name = models.CharField(max_length = 80)
     admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
@@ -16,26 +23,7 @@ class Neighbourhood(models.Model):
 
     def __str__(self):
         return self.name
-
-    def save_image(self):
-        self.save()
-
-    def delete_image(self):
-        self.delete()
-
-class Occupant(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-    email = models.EmailField()
-    profile_pic = CloudinaryField('Profile Picture')
-
-    def __str__(self):
-        return self.user.username
-
-    def save_profile(self):
-        self.save()
-
-    def delete_profile(self):
-        self.delete() 
+ 
 
 class Business(models.Model):
     name = models.CharField(max_length = 80)
--- neighbourhoodApp/tests.py
+++ neighbourhoodApp/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Admin, Amenity, Post, Neighbourhood, Occupant, Business
 
-# Create your tests here.
+class AdminTestClass(TestCase):
+    def setUp(self):
+        self.lorna = User(username = "lorna", email = "lorna@gmail.com",password = "1234")
+        self.admin = Admin(user= self.lorna)
+        self.lorna.save()
+        self.admin.save()
+
+    def tearDown(self):
+        Admin.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.lorna, User))
+        self.assertTrue(isinstance(self.admin, Admin))
+
+class OccupantTestClass(TestCase):
+    def setUp(self):
+        self.lorna = User(username = "lorna", email = "lorna@gmail.com",password = "1234")
+        self.occupant = Occupant(user= self.lorna, profile_pic='mepng')
+        self.lorna.save()
+        self.occupant.save()
+
+    def tearDown(self):
+        Occupant.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.lorna, User))
+        self.assertTrue(isinstance(self.occupant, Occupant))
