01c96adb861883fb2c786a8fbcac16933d6a13f5
Lorna Kamau <kamaulorna@gmail.com>
2020-07-19 10:28:31 +0300

    create neighbourhood model

--- neighbourhoodApp/models.py
+++ neighbourhoodApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length = 80)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    occupants = models.ManyToManyField(Profile, related_name="occupant")
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
