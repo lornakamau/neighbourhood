7c2510cf2ef36cdef75e1355bfbe810df6c8b760
Lorna Kamau <kamaulorna@gmail.com>
2020-07-19 23:36:50 +0300

    add create profile admin template

--- neighbourhoodApp/urls.py
+++ neighbourhoodApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     url(r'^$',views.home,name = 'home'),
     url(r'^email/$',views.email,name = 'email'),
+     url(r'^create_profile_admin/$',views.create_profile_admin,name='create_profile_admin'),
 ]
